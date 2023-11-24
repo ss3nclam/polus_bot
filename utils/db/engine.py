b0d386d87bcd93f78dcfb7aec85d15d5ab85eb66
Gleb Rodionov <137335455+ss3nclam@users.noreply.github.com>
2023-11-24 16:50:19 +0400

    updated

--- utils/db/engine.py
+++ utils/db/engine.py
@@ -3,5 +3,5 @@ from sqlalchemy import create_engine
 
 def create(config):
     db_cred = dict(config.items('DATABASE'))
-    user, pswrd, host, db_name = [db_cred[key] for key in db_cred]
-    return create_engine(f"mysql+mysqldb://{user}:{pswrd}@{host}/{db_name}")
+    user, pswrd, host, port, db_name = [db_cred[key] for key in db_cred]
+    return create_engine(f"mysql+mysqldb://{user}:{pswrd}@{host}:{port}/{db_name}")
